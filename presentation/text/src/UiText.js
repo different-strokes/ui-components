a4a6b2adf38b0a818885123024e150a3ac2b164b
Fredrik Stave <fredrik.stave@e24.no>
2018-01-29 20:29:35 +0100

    feat(@strokes/ui-text): updated the theme and markup

--- presentation/text/src/UiText.js
+++ presentation/text/src/UiText.js
@@ -2,17 +2,20 @@ import React from 'react';
 import PropTypes from 'prop-types';
 // import { articleComponentPropTypes } from '@strokes/article-component-prop-types';
 
-const UiText = ({ text }) => (
-  <div>
-    <p>This is UiText. {text}</p>
-  </div>
+const UiText = ({ text, theme }) => (
+  <p className={theme.root}>{text}</p>
 );
 
 UiText.propTypes = {
   // text: articleComponentPropTypes.text.isRequired,
   text: PropTypes.string.isRequired,
+  theme: PropTypes.shape({
+    root: PropTypes.string,
+  }).isRequired,
 };
 
-UiText.defaultProps = {};
+UiText.defaultProps = {
+  theme: {},
+};
 
 export default UiText;
